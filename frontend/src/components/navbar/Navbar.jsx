23d26bce07c11f11091b014b87343f6ce7dcfedb
ashishsen003 <ashishsen997@gmail.com>
2023-12-16 22:44:53 +0530

    added universal font in App.css

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../../assets/logo.jpg'
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <section id='navbar'>
+
+        <div className='logo__div'>
+            <img src={logo} alt="" />
+        </div>
+
+        <p>Home</p>
+        <p>Subjects</p>
+
+        <div className='search__div'>
+            <FaSearch />
+            <input type="text" placeholder="find it here" id="" />
+        </div>
+
+        <div>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>SignUp</Link>
+        </div>
+
+    </section>
+  )
+}
+
+export default Navbar
