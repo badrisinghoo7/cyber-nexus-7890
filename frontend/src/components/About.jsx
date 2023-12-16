036e1ed12955553d5675b44752ce6a5a16653092
Diptajit Das <diptajitdas66@gmail.com>
2023-12-16 23:56:31 +0530

    third

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -70,4 +70,29 @@ const AboutUs = () => {
             gemstones and materials.
           </p>
         </JewelryCollectionDescription>
-     
+        <List spacing={3}>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color="green.500" />
+            Exquisite Gemstones
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color="green.500" />
+            Handcrafted Jewelry
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color="green.500" />
+            Unique Designs
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color="green.500" />
+            Quality Craftsmanship
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color="green.500" />
+            Personalized Service
+          </ListItem>
+        </List>
+        <JewelryCollectionImage>
+          <img src="https://plus.unsplash.com/premium_photo-1661645473770-90d750452fa0?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Jewelry Collection" />
+        </JewelryCollectionImage>
+    
