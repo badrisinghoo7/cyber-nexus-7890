c7d87b4c28466ba84ed156347763399d76a374d7
Diptajit Das <diptajitdas66@gmail.com>
2023-12-18 01:58:54 +0530

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-
+// import Footer from './components/footer/Footer';
+// import Navbar from './components/navbar/Navbar';
+import SubjectList from './components/subjects/subjectList';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Footer />
+      {/* <Navbar /> */}
+      {/* <Footer /> */}
+      <SubjectList/>
     </div>
   );
 }
